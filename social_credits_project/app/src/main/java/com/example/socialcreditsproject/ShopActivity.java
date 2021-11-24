b86cdf9f5d08a8e7c643b75a3d57cde87b54cd6e
Cat Crusader <cat.crusader31@gmail.com>
2021-11-24 22:36:20 +0200

    add recycler view on shop page

--- social_credits_project/app/src/main/java/com/example/socialcreditsproject/ShopActivity.java
+++ social_credits_project/app/src/main/java/com/example/socialcreditsproject/ShopActivity.java
@@ -1,14 +1,34 @@
 package com.example.socialcreditsproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class ShopActivity extends AppCompatActivityManager {
 
+    String s1[],s2[];
+    int images[]={R.drawable.img_2,
+                  R.drawable.img_1,
+                  R.drawable.img_3,
+                  R.drawable.img};
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        recyclerView = findViewById(R.id.recycler_view_shop);
+
+        s1 = getResources().getStringArray(R.array.shop_items);
+        s2 = getResources().getStringArray(R.array.shop_items_description);
+
+
+
+        RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this,s1,s2,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
